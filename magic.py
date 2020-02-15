2a72bed2f1938389c0ab6edab7408ddb8d7bd2a2
Ashutosh Kushwah <ashworld12136@gmail.com>
2020-02-16 04:52:20 +0530

    Add files via upload

---
+++ magic.py
@@ -0,0 +1,13 @@
+import random
+
+class Spell:
+    def __init__(self, name, cost, dmg, type1):
+        self.name = name
+        self.cost = cost
+        self.dmg = dmg
+        self.type1 = type1
+
+    def gen_dmg(self):
+        low = self.dmg - 15
+        high = self.dmg + 15
+        return random.randrange(low, high)
